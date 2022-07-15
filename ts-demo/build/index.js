0a0715eb8ae9154c1928136387b35cd113c7ab46
Alfonso, Montes de Oca <97349912+AlfonsoM0@users.noreply.github.com>
2022-07-14 22:43:12 -0300

    Ts basic Template

---
+++ ts-demo/build/index.js
@@ -0,0 +1,7 @@
+"use strict";
+console.log('Hola Typescript');
+// comentario
+/**
+ * Comentario de documentación.
+ * comentario multilínea.
+ */
---
+++ ts-demo/src/index.ts
@@ -0,0 +1,8 @@
+console.log('Hola Typescript');
+
+// comentario
+
+/**
+ * Comentario de documentación.
+ * comentario multilínea.
+ */
